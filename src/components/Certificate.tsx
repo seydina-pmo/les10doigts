171d02b4f71fed32f78e1ae045f6b8ae526353f2
Seydina Mouhamadou SARR <seydina.sarr@dakar2026.org>
2026-09-21 19:08:09 +0000

    fix: conflit variable imgData certificat

--- src/components/Certificate.tsx
+++ src/components/Certificate.tsx
@@ -247,15 +247,15 @@ export function CertificateDownloadButton({
       tmpCanvas.height = sigImg.height;
       const tmpCtx = tmpCanvas.getContext("2d")!;
       tmpCtx.drawImage(sigImg, 0, 0);
-      const imgData = tmpCtx.getImageData(0, 0, tmpCanvas.width, tmpCanvas.height);
-      const d = imgData.data;
+      const sigPixels = tmpCtx.getImageData(0, 0, tmpCanvas.width, tmpCanvas.height);
+      const d = sigPixels.data;
       for (let i = 0; i < d.length; i += 4) {
         // If pixel is near-white (R>220, G>220, B>220), make transparent
         if (d[i] > 220 && d[i + 1] > 220 && d[i + 2] > 220) {
           d[i + 3] = 0; // set alpha to 0
         }
       }
-      tmpCtx.putImageData(imgData, 0, 0);
+      tmpCtx.putImageData(sigPixels, 0, 0);
       ctx.drawImage(tmpCanvas, W - 350 - sigW / 2, bottomY - sigH - 10, sigW, sigH);
       // "Le Directeur Général" text under signature
       ctx.fillStyle = "#1A1A2E";
